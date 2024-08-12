2039270a8da85b16bc707036a6c5e8880a43da8e
IlaiAzaria <ilai.bagab@gmail.com>
2024-08-12 13:07:43 +0300

    Initial commit

---
+++ src/main/java/org/chessio/chessio_server/ChessioServerApplication.java
@@ -0,0 +1,13 @@
+package org.chessio.chessio_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessioServerApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ChessioServerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/chessio/chessio_server/ChessioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chessio.chessio_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
